61ad91e0340450393dce16aae6f92907c5205b36
yugeonkwak <ts22003@ts.hs.kr>
2023-05-01 09:51:57 +0900

    yugeonkwak

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.write(dir(st))
 
 st.help(range)
 
-st.title(' Data Display Elements')
+'''st.title(' Data Display Elements')
 st.header('Iris data display')
 
 df=pd.read_csv('iris.csv')
@@ -35,13 +35,14 @@ st.subheader('Method 2')
 st.dataframe(df.style.hightlight_max(axis=0))
 
 st.subheader('Method 3: static table')
-st.table(df)
+st.table(df)'''
 
-st.header('display json')
+'''st.header('display json')
 st.json({'data': 'name'})
 
 st.header('display code')
 st.code('''
 def sayHello():
     print('Hello Streamlit')
-''', language='python')
+''', language='python')'''
+
