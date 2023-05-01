03265295ca7db74e68d589dde7221c6fcf82bb1a
yugeonkwak <ts22003@ts.hs.kr>
2023-05-01 09:50:19 +0900

    yugeonkwak

--- app.py
+++ app.py
@@ -24,18 +24,18 @@ st.write(dir(st))
 
 st.help(range)
 
-#st.title(' Data Display Elements')
-#st.header('Iris data display')
+st.title(' Data Display Elements')
+st.header('Iris data display')
 
-#df=pd.read_csv('iris.csv')
-#st.subheader('Method 1')
-#st.dataframe(df)
+df=pd.read_csv('iris.csv')
+st.subheader('Method 1')
+st.dataframe(df)
 
-#st.subheader('Method 2')
-#st.dataframe(df.style.hightlight_max(axis=0))
+st.subheader('Method 2')
+st.dataframe(df.style.hightlight_max(axis=0))
 
-#st.subheader('Method 3: static table')
-#st.table(df)
+st.subheader('Method 3: static table')
+st.table(df)
 
 st.header('display json')
 st.json({'data': 'name'})
