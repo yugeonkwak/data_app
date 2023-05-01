835a18539323c57ec20988e15c76b906f8424564
yugeonkwak <ts22003@ts.hs.kr>
2023-05-01 09:54:35 +0900

    yugeonkwak

--- app.py
+++ app.py
@@ -46,3 +46,7 @@ def sayHello():
     print('Hello Streamlit')
 ''', language='python')'''
 
+col1, col2, col3=st.columns(3)
+col1.metric('Temperature','70 F','1.2 F')
+col2.metric('Wind','9 mph','-8%')
+col3.metric('Humidity','86%','4%')
