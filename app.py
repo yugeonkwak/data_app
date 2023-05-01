284fc0ff210d766b544c1256a68615d105ad078d
yugeonkwak <ts22003@ts.hs.kr>
2023-05-01 09:37:28 +0900

    yugeonkwak

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title('This is a title')
 st.header('This is a header')
@@ -14,7 +15,7 @@ st.success('성공적으로 저장하였습니다.')
 st.warning('업로드할 수 없는 파일 형식입니다.')
 st.info('알려드립니다.')
 st.error('This is an error')
-st.exception('This is an exception  ')
+st.exception('This is an exception')
 
 st.write('Normal Text')
 st.write('## This is a markdown text')
@@ -22,3 +23,16 @@ st.write(1+2)
 st.write(dir(st))
 
 st.help(range)
+
+st.title(' Data Display Elements')
+st.header('Iris data display')
+
+df=pd.read_csv('iris.csv')
+st.subheader('Method 1')
+st.dataframe(df)
+
+st.subheader('Method 2')
+st.dataframe(df.style.hightlight_max(axis=0))
+
+st.subheader('Method 3: static table')
+st.table(df)
