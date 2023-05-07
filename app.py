3c2904a6355e611e324c97d1a7cfd0d7985eb324
yugeonkwak <ts22003@ts.hs.kr>
2023-05-07 23:06:28 +0900

    yugeonkwak

--- app.py
+++ app.py
@@ -77,3 +77,13 @@ choice=st.selectbox('Language',my_lang)
 st.write('You selected %s'%choice)
 
 multi_choices=st.multiselect('Multi Select',my_lang)
+
+age=st.slider('How old are you?',1,120,18)
+st.write('I\'m ',age,'years old')
+
+values=st.slider('Select a range of values',0.0,100.0,(25.0,75.0))
+st.write('Values:',values)
+
+from datetime import time
+appointment=st.slider('Schedule your appointment:',value=(time(11,30),time(12,45)))
+st.write('You\'re scheduled for:',appointment)
