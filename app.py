ff83f6dab596e3c58dbc39008711a077a2d37012
yugeonkwak <ts22003@ts.hs.kr>
2023-05-08 08:29:15 +0900

    yugeonkwak

--- app.py
+++ app.py
@@ -87,3 +87,7 @@ st.write('Values:',values)
 from datetime import time
 appointment=st.slider('Schedule your appointment:',value=(time(11,30),time(12,45)))
 st.write('You\'re scheduled for:',appointment)
+
+from datetime import datetime
+start_time=st.slider('When do you start?',value=datetime(2020,1,1,9,30),format='MM/DD/YY - hh:mm')
+st.write('Start time:',start_time)
